4125104dee86e6a97809df0b53d82b08380a5ea0
Ryan Bratton <ryanbratton@Ryans-iMac.lan>
2020-10-18 18:35:19 -0500

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+import FullPost from './FullPost/FullPost'; 
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/" exact>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
